cfdea09264adf08a03be53d3c305e7f85f5c79e2
tanish <tanishdeswal1202@gmail.com>
2026-08-01 14:45:58 +0530

    Configure production frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL:
-        import.meta.env.VITE_API_URL
-
+    baseURL: import.meta.env.VITE_API_URL
 });
+
+export default api;
 
 api.interceptors.request.use((config) => {
 
