4af488057880a51774bbcce4e61b4a74edac8634
tanish <tanishdeswal1202@gmail.com>
2026-08-04 10:27:07 +0530

    fix the uploading bar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { UploadProvider } from "./context/UploadContext";
+import { UploadProvider } from "./components/UploadContext";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
